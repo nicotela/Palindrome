aaad78ba4874a8faa1a57acf53197d77e5053ab8
Nicol Gotzova <nicol.gotzova@gmail.com>
2021-12-28 22:34:53 +0100

    normalize + kontrola delky poli

--- Palindrome.py
+++ Palindrome.py
@@ -1,29 +1,33 @@
 import sys, unicodedata
 
-palindrome = sys.argv[1].lower().replace(" ","") #nemusi byt kulate zavorky, neni funkce
-palindrome = unicodedata.normalize("NFKD", palindrome)
-palindrome_bez_diakritiky = ''
-for znak in palindrome:
-    if not unicodedata.combining(znak):
-        palindrome_bez_diakritiky += znak
+def normalize (input_string, to_lower_case=True, remove_spaces=True, remove_accents=True):
+    if to_lower_case:
+        input_string = input_string.lower()
+    if remove_spaces:
+        input_string = input_string.replace(" ","")
+    if remove_accents:
+        input_string = unicodedata.normalize("NFKD", input_string)
+        palindrome_bez_diakritiky = ''
+        for znak in input_string:
+            if not unicodedata.combining(znak):
+                palindrome_bez_diakritiky += znak
+        input_string = palindrome_bez_diakritiky        
+    return input_string
 
 def is_palindrome (palindrome_bez_diakritiky):
     for index,letter in enumerate(palindrome_bez_diakritiky):    
-        #print (index,letter)
-        #print (f"{letter} ? {palindrome [index*-1-1]}")
         if letter == (palindrome_bez_diakritiky [index*-1-1]):
             pass
         else: 
-            return "není"
-    return "je"
+            return False
+    return True
 
+if (len(sys.argv)) == 1:
+    print ("\n\n!!! Nebyl zadán vstupní parametr !!!\n\n")
+    
+palindrome = sys.argv[1]
+
+palindrome_bez_diakritiky = normalize (palindrome)
 result = is_palindrome (palindrome_bez_diakritiky)
-print ("Toto", result, "palindrom.")
-
-
-
-
-
-
-
+print (f"Zadaný řetězec: {palindrome} ({palindrome_bez_diakritiky}) {'je' if result else 'není'} palindrom.")
 
